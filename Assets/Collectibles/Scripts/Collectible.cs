c040b13fce392de40b909cda0ba4b1b0e655cc03
lormee94 <35290589+lormee94@users.noreply.github.com>
2020-09-12 15:26:15 +0800

    Basic Collectible sprite/script/prefab - Extra light related assets like pipeline

---
+++ Assets/Collectibles/Scripts/Collectible.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    float originalY;
+    public float hoverIntensity = 0.5F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //Floating animation
+        transform.position = new Vector3(transform.position.x,
+            originalY + (Mathf.Sin(Time.time) * hoverIntensity),
+            transform.position.z);
+    }
+
+    // public void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     PlayerController controller = other.GetComponent<PlayerController>();
+
+    //     if (controller != null)
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    // }
+}
---
+++ Assets/Collectibles/Scripts/HealthCollectible.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : Collectible
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // public void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     PlayerController controller = other.GetComponent<PlayerController>();
+
+    //     if (controller != null)
+    //     {
+    //         //controller.changeHealth(5);
+    //     }
+
+    //     base.OnTriggerEnter2D(other);
+    // }
+}
