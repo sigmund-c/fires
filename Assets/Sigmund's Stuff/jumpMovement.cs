8e90cec858348a3007d5d8092836e93772fa732c
no-10 <e0576183@u.nus.edu>
2020-09-12 21:47:43 +0800

    Movement control by mouse (meter style)

--- Assets/Sigmund's Stuff/jumpMovement.cs
+++ Assets/Sigmund's Stuff/jumpMovement.cs
@@ -11,6 +11,7 @@ public class jumpMovement : MonoBehaviour
 
     Rigidbody2D rb;
     Transform sprite;
+    Vector2 shootingTriangle = new Vector2(0, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -23,43 +24,53 @@ public class jumpMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("w") && !isJumping)
+        
+        if (Input.GetMouseButtonDown(0) && !isJumping)
         {
             jumpStart = Time.time;
             sprite.localScale = new Vector3(1, 0.5f, 1);
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Vector2 mousePosition = ray.GetPoint(3);
+            Vector2 playerPosition = rb.transform.position;
+            shootingTriangle = (mousePosition - playerPosition).normalized;
         }
-        if (Input.GetKeyUp("w") && !isJumping)
+        if (Input.GetMouseButtonUp(0) && !isJumping)
         {
-            Jump(Time.time - jumpStart);
+            Jump(Time.time - jumpStart, shootingTriangle);
         }
-        if (Input.GetKey("w") && Time.time - jumpStart > 2f && !isJumping)
+        if (Input.GetMouseButton(0) && Time.time - jumpStart > 2f && !isJumping)
         {
-            Jump(Time.time - jumpStart);
+            Jump(Time.time - jumpStart, shootingTriangle);
         }
+
     }
 
-    void Jump(float jumpAmount)
+    void Jump(float jumpAmount, Vector2 dirc)
     {
         if (jumpAmount > 2)
         {
             jumpAmount = 2f;
         }
 
-
         isJumping = true;
         sprite.localScale = new Vector3(1, 1, 1);
+        rb.AddForce(dirc * 10 * jumpAmount, ForceMode2D.Impulse);
+
+        /*
         if (Input.GetKey("a") && !Input.GetKey("d"))
         {
-            rb.AddForce(Quaternion.Euler(0, 0, 30) * transform.up * jumpAmount * 350);
+            rb.AddForce(new Vector2(1f, 5f),ForceMode2D.Impulse);
             Debug.Log("jump left");
         } else if (!Input.GetKey("a") && Input.GetKey("d"))
         {
-            rb.AddForce(Quaternion.Euler(0, 0, -30) * transform.up * jumpAmount * 350);
+            rb.AddForce(Quaternion.Euler(0, 0, -45) * transform.up * jumpAmount * 350);
             Debug.Log("jump right");
         } else
         {
             rb.AddForce(transform.up * jumpAmount * 350);
-        }
+        }*/
 
     }
 
