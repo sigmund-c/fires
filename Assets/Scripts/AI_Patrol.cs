3d3bbbc82106d82e613f50747ea12b12580cd7e6
Sigmund <sigmundchianasta@gmail.com>
2020-09-21 18:04:35 +0800

    add bear enemy

---
+++ Assets/Scripts/AI_Patrol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Patrol : MonoBehaviour
+{
+    public float moveSpeed;
+    public float maxFallDist = 2f;
+    public float wallDetectionDist = 0.1f;
+
+    public Transform groundDetector;
+
+    private bool movingLeft = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetector.position, Vector2.down, maxFallDist);
+        if (groundInfo.collider == false) // No ground found
+        {
+            Debug.Log("no floor");
+            Rotate();
+        } else // Check for obstructions/walls
+        {
+            RaycastHit2D wallInfo = Physics2D.Raycast(groundDetector.position, Vector2.left, wallDetectionDist);
+            if (wallInfo.collider == true && wallInfo.collider.gameObject.tag == "ground")
+            {
+                Debug.Log(wallInfo.collider.gameObject.tag);
+                Rotate();
+            }
+        }
+    }
+
+    void Rotate()
+    {
+        if (movingLeft == true) // Moving Left
+        {
+            transform.Rotate(0, 180, 0);
+            movingLeft = false;
+        }
+        else // Moving Right
+        {
+            transform.Rotate(0, 180, 0);
+            movingLeft = true;
+        }
+    }
+}
