722b4bcb5d87e38752eb8ec30b68d39ad1fd7427
no-10 <e0576183@u.nus.edu>
2020-09-25 12:50:55 +0800

    Add checkpoint
    Now can only save the position of the player.

---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Save
+{
+    public List<float> livingTargetPositionsX = new List<float>();
+    public List<float> livingTargetPositionsY = new List<float>();
+    public List<int> livingTargetsTypes = new List<int>();
+
+    public int hits = 0;
+    public int shots = 0;
+}
