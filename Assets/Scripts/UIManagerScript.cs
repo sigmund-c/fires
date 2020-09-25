72b80cff2ad5b78caf777150647740e7a437852c
no-10 <e0576183@u.nus.edu>
2020-09-25 11:34:16 +0800

    Revert "Revert "Add pause menu and change main menu.""
    This reverts commit 5fbe63d3a41c422cfd2c42e5c390c7215e430999.

---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FireBoiSampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public GameObject pauseMenu;
+
+    public void OnPause()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void OnResume()//点击“回到游戏”时执行此方法
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void OnMainMenu()//点击“重新开始”时执行此方法
+    {
+        //Loading Scene0
+        SceneManager.LoadScene("MenuScene");
+        Time.timeScale = 1f;
+    }
+}
