726edcb93002d0b2f90637bb90a1dfe2ef61b684
Sigmund <sigmundchianasta@gmail.com>
2020-10-19 17:51:27 +0800

    used new health in old stages, minor fixes

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    public float speed = 0.5f;
 
     public Transform[] snaps;
     public float[] snapDistances;
 
-    private Transform target;
+    private bool hasOverrideTarget = false;
+    private Vector3 overrideTarget;
+    private Vector3 target;
     private Camera cam;
+
+    private float shakeMagnitude;
+    private Vector3 initialPosition;
+    private float shakeDuration = 0f;
 
     private void Start()
     {
@@ -18,28 +24,78 @@ public class CameraFollow : MonoBehaviour
         player = GameObject.Find("Player").transform;
     }
 
+    void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+
+            shakeDuration -= Time.deltaTime;
+        }
+    }
+
     // Update is called once per frame
     void LateUpdate()
     {
-        if (player != null)
+        if (shakeDuration <= 0)
         {
-            target = player;
+            DecideTarget();
+            transform.position = Vector3.MoveTowards(transform.position, target + new Vector3(0, 0, -10f), 1f);
 
-            for (int i = 0; i < snaps.Length; i++)
-            {
-                float snapDistance = 5;
-                if (i < snapDistances.Length)
-                {
-                    snapDistance = snapDistances[i];
-                }
-                if (Vector2.Distance(player.position, snaps[i].position) < snapDistance)
-                {
-                    target = snaps[i];
-                    // cam.orthographicSize = snapDistance;
-                }
-            }
-
-            transform.position = Vector3.MoveTowards(transform.position, target.position + new Vector3(0, 0, -10f), speed);
         }
+    }
+
+    void DecideTarget()
+    {
+        if (hasOverrideTarget)
+        {
+            target = overrideTarget;
+            return;
+        }
+
+        for (int i = 0; i < snaps.Length; i++)
+        {
+            float snapDistance = 5;
+            if (i < snapDistances.Length)
+            {
+                snapDistance = snapDistances[i];
+            }
+            if (Vector2.Distance(player.position, snaps[i].position) < snapDistance)
+            {
+                target = snaps[i].position;
+                cam.orthographicSize = snapDistance;
+                return;
+            }
+        }
+
+        target = player.position;
+        return;
+    }
+
+    public void FocusOn(Vector2 position, float duration)
+    {
+        overrideTarget = position;
+        hasOverrideTarget = true;
+        StartCoroutine(NullOverrideTarget(duration));
+    }
+
+    public void FocusOn(int cameraSnapIndex, float duration)
+    {
+        overrideTarget = snaps[cameraSnapIndex].position;
+        hasOverrideTarget = true;
+        StartCoroutine(NullOverrideTarget(duration));
+    }
+
+    IEnumerator NullOverrideTarget(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        hasOverrideTarget = false;
+    }
+
+    public void TriggerShake(float magnitude, float duration)
+    {
+        initialPosition = transform.position;
+        shakeMagnitude = magnitude;
+        shakeDuration = duration;
     }
 }
