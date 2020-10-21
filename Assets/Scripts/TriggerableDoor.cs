9744aad71242d2d7ed8fd7bc1a3e7a4dbef75dc0
marvintxd <marvin-tan@hotmail.com>
2020-10-21 23:58:31 +0800

    Add trigger switches (torches) and doors, update levels

---
+++ Assets/Scripts/TriggerableDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerableDoor : Triggerable
+{
+    private Collider2D collider;
+    private SpriteRenderer sr;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+        sr = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public override void Trigger()
+    {
+        audioSource.Play();
+        StartCoroutine(DelayedTrigger());
+    }
+
+    IEnumerator DelayedTrigger()
+    {
+        yield return new WaitForSeconds(0.5f);
+        collider.enabled = false;
+        sr.color = new Color(0.4f, 0.4f, 0.4f, 0.5f);
+    }
+}
