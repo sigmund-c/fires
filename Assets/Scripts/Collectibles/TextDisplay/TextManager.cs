71f8dbbbc70a3b59d5165937a3f81c99807c8c7f
lormee94 <35290589+lormee94@users.noreply.github.com>
2020-10-31 21:02:05 +0800

    Read text display popup

---
+++ Assets/Scripts/Collectibles/TextDisplay/TextManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    public Image textBox;
+    public Text bodyText;
+    public TextStorage textStorage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enable(false);
+    }
+
+    public void Enable(bool val)
+    {
+        textBox.enabled = val;
+        bodyText.enabled = val;
+    }
+
+    public void SetBody(int i)
+    {
+        bodyText.text = textStorage.Retrieve(i);
+    }
+}
