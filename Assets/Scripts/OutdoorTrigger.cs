4bea76944ee064d0b80832024721fabca25e062c
marvintxd <marvin-tan@hotmail.com>
2020-10-31 15:12:27 +0800

    Improve cave/sky bg on 2-2, new platforms, level changes

---
+++ Assets/Scripts/OutdoorTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutdoorTrigger : MonoBehaviour
+{
+    public ParticleSystem[] snowParticles;
+    public GameObject cavesBg;
+    public GameObject skyBg;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            print("Player outside");
+            foreach(ParticleSystem p in snowParticles)
+            {
+                // p.SetActive(true);
+                p.Stop();
+            }
+
+            cavesBg.SetActive(false);
+            // skyBg.SetActive(true);
+
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            print("Player inside");
+            foreach(ParticleSystem p in snowParticles)
+            {
+                // p.SetActive(false);
+                p.Play();
+            }
+
+            cavesBg.SetActive(true);
+            // skyBg.SetActive(false);
+
+        }
+    }
+}
