5774427a9a130e28c9d949eedc676cf535fa2607
lormee94 <35290589+lormee94@users.noreply.github.com>
2020-11-08 16:54:55 +0800

    Fix crystalboss icicle unable to damage

---
+++ Assets/Scripts/Player and Enemies/CrystalBoss/Icicle_CrystalBoss.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icicle_CrystalBoss : MonoBehaviour
+{
+    new Collider2D collider;
+
+    void Awake()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Damageable dmg = col.gameObject.GetComponent<Damageable>();
+        if (dmg != null)
+        {
+            dmg.TriggerCollision(collider);
+        }
+    }
+}
