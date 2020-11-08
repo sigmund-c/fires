0ccb773b0ade19b2f0c103b2c8acb20e8710aa55
marvintxd <marvin-tan@hotmail.com>
2020-11-09 05:25:28 +0800

    Ending sequence timing change

--- Assets/Scripts/Slideshow.cs
+++ Assets/Scripts/Slideshow.cs
@@ -7,6 +7,8 @@ public class Slideshow : MonoBehaviour
 {
     public GameObject[] sprites;
     public float[] displayTimes;
+    public float[] fadeTimes;
+    public float delay;
 
     // Start is called before the first frame update
     void Start()
@@ -16,32 +18,33 @@ public class Slideshow : MonoBehaviour
 
     IEnumerator DisplaySprites()
     {
+        yield return new WaitForSeconds(delay);
         for (int i = 0; i < sprites.Length; i++)
         {
             sprites[i].SetActive(true);
             SpriteRenderer sr = sprites[i].GetComponent<SpriteRenderer>();
             yield return StartCoroutine(FadeIn(sr));
             yield return new WaitForSeconds(displayTimes[i]);
-            yield return StartCoroutine(FadeOut(sr));
+            yield return StartCoroutine(FadeOut(sr, fadeTimes[i]));
         }
         SceneManager.LoadScene("MenuScene", LoadSceneMode.Single);
     }
 
     IEnumerator FadeIn(SpriteRenderer sr)
     {
-        for (int i = 1; i <= 50; i++)
+        for (int i = 1; i <= 60; i++)
         {
-            sr.color = Color.Lerp(Color.black, Color.white, 1f/50 * i);
-            yield return new WaitForSeconds(0.04f);
+            sr.color = Color.Lerp(Color.black, Color.white, 1f/60 * i);
+            yield return new WaitForSeconds(0.033f);
         }
     }
 
-    IEnumerator FadeOut(SpriteRenderer sr)
+    IEnumerator FadeOut(SpriteRenderer sr, float time)
     {
-        for (int i = 1; i <= 50; i++)
+        for (int i = 1; i <= 60; i++)
         {
-            sr.color = Color.Lerp(Color.white, Color.black, 1f/50 * i);
-            yield return new WaitForSeconds(0.03f);
+            sr.color = Color.Lerp(Color.white, Color.black, 1f/60 * i);
+            yield return new WaitForSeconds(time/60);
         }
     }
 }
